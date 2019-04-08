4360a897aba954348c09b093dbe3760bb39a4fc3
Eduard <eduardboschbertran@gmail.com>
2019-04-08 14:22:55 +0200

    config: Add carthage support

---
+++ BABFrameObservingInputAccessoryView/BABFrameObservingInputAccessoryView/BABFrameObservingInputAccessoryView_project.h
@@ -0,0 +1,19 @@
+//
+//  BABFrameObservingInputAccessoryView.h
+//  BABFrameObservingInputAccessoryView
+//
+//  Created by Eduard Bosch on 08/04/2019.
+//  Copyright © 2019 Bryn Bodayle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BABFrameObservingInputAccessoryView.
+FOUNDATION_EXPORT double BABFrameObservingInputAccessoryViewVersionNumber;
+
+//! Project version string for BABFrameObservingInputAccessoryView.
+FOUNDATION_EXPORT const unsigned char BABFrameObservingInputAccessoryViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BABFrameObservingInputAccessoryView/PublicHeader.h>
+
+
